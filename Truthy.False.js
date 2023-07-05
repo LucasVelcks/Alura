ca47c25f733901a0799b2876039ad07d8224a3ce
Lucas Nascimento <lucasvelcks@gmail.com>
2023-07-04 22:04:17 -0300

    Truthy e False

---
+++ Truthy.False.js
@@ -0,0 +1,20 @@
+// 0 = False
+// 1 = True
+
+//console.log(0 == false)
+//console.log ("" == false)
+
+// Undefined = Valor indefinido
+// Null = Vazio ou nada
+
+let minhaVar;
+let varNull = null;
+//console.log(minhaVar)
+//console.log(varNull)
+console.log(typeof minhaVar)
+console.log (typeof varNull)
+
+let numero = 3;
+let texto = "ALURA";
+//console.log(typeof numero)
+//console.log (typeof texto)
