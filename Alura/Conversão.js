3331ab246830e21c6f72219fadf7b4001551e249
Lucas Nascimento <lucasvelcks@gmail.com>
2023-07-07 21:31:29 -0300

    atualizando comentarios

--- Alura/Conversão.js
+++ Alura/Conversão.js
@@ -1,7 +1,7 @@
 // Conversão
 
-// number()
-// String()
+// number() - CONVERTE STRING PARA NUMBER
+// String() - CONVERTE NUMBRA PARA STRING
 
 const number = 277;
 const numString = "357";
@@ -14,6 +14,7 @@ const numString1 = "472";
 console.log(number1 + Number(numString1));
 
 // PODE SER FEITO DIRETAMENTE NA DECLARAÇÃO
+// SÓ PODEMOS CONVERTER UMA STRING PARA NUMBER SE HOUVER APENAS NUMEROS NESSA STRING.
 
 const numero = 123;
 const numTexto = Number("123");
