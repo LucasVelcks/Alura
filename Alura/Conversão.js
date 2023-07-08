5c6dce9c7356faf757f0fbd0457b45ac08cd1294
Lucas Nascimento <lucasvelcks@gmail.com>
2023-07-07 21:25:17 -0300

    Conversão

---
+++ Alura/Conversão.js
@@ -0,0 +1,21 @@
+// Conversão
+
+// number()
+// String()
+
+const number = 277;
+const numString = "357";
+
+console.log(number + numString);
+
+const number1 = 367;
+const numString1 = "472";
+
+console.log(number1 + Number(numString1));
+
+// PODE SER FEITO DIRETAMENTE NA DECLARAÇÃO
+
+const numero = 123;
+const numTexto = Number("123");
+
+console.log(numero + numTexto);
