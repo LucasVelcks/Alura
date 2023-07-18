7557a6a917a13b2bc9dba1342511e36c3fe37369
Lucas Nascimento <lucasvelcks@gmail.com>
2023-07-18 19:56:34 -0300

    Comparação

---
+++ Alura/Comparação.js
@@ -0,0 +1,12 @@
+const numero = 5;
+const texto = "5";
+
+// == (Comparação imolicita - só compara o valor)
+console.log(numero == texto);
+
+// === (Comparação explicita - compara o valor e o tipo de dado)
+console.log(numero === texto); 
+
+// Typeof = Pergunta qual tipo de dado está sendo armazenado dentro da variavel
+console.log(typeof numero);
+console.log(typeof texto);
