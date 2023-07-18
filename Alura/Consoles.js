32b5cf034ccdd9a10e6adc961060129830a38693
Lucas Nascimento <lucasvelcks@gmail.com>
2023-07-17 23:25:41 -0300

    Tipos de Console ( )

---
+++ Alura/Consoles.js
@@ -0,0 +1,11 @@
+// console.log();
+// console.error();
+// console.table();
+// console.time e console.timeEnd();
+// console.trace();
+
+ console.log("deu erro");
+ console.error("deu erro");
+
+console.log("deu erro");
+console.error(new Error("deu erro"));
