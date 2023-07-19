954aff16205f9c6d12f783ea302407b85d4a8e44
Lucas Nascimento <lucasvelcks@gmail.com>
2023-07-19 20:57:04 -0300

    Operador ternário e concatenação

---
+++ Alura/Concatenação.js
@@ -0,0 +1,10 @@
+const nome = "Lucas";
+const idade = 24;
+const sobrenome = "Nascimento";
+const cidade = "Santos"
+
+const apresentação = "Meu nome é" + nome + sobrenome + ", tenho" + idade + "anos de idade" + 
+"e moro na ciadade de" + cidade;
+
+console.log(apresentação);
+
