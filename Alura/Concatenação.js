09c7f2dbf10fa928d1db1cbb0f3e7bb9e714675d
Lucas Nascimento <lucasvelcks@gmail.com>
2023-07-19 21:07:17 -0300

    corrigindo

--- Alura/Concatenação.js
+++ Alura/Concatenação.js
@@ -3,8 +3,8 @@ const idade = 24;
 const sobrenome = "Nascimento";
 const cidade = "Santos"
 
-const apresentação = "Meu nome é" + nome + sobrenome + ", tenho" + idade + "anos de idade" + 
-"e moro na ciadade de" + cidade;
+const apresentação = "Meu nome é " + nome + " " + sobrenome + ", tenho " + idade + " anos de idade" + 
+" e moro na ciadade de " + cidade + ".";
 
 console.log(apresentação);
 
