ba4d989352ae584866f8afcf80bac79959ad8ee1
Lucas Nascimento <lucasvelcks@gmail.com>
2023-07-25 21:30:37 -0300

    Parâmetros de função

---
+++ Alura/Parâmetros.js
@@ -0,0 +1,17 @@
+function soma(numero1, numero2) {
+return numero1 + numero2
+}
+
+console.log(soma (2, 2))
+console.log(soma (312, 34))
+console.log(soma (-34, 42));
+
+function nomeIdade (nome, idade){
+    return `meu nome é ${nome} minha idade é ${idade}`
+}
+
+console.log(nomeIdade ('Lucas', 24))
+
+function soma (numero3, numero4) {
+    return numero3 + numero4
+}
