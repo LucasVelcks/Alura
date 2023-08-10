3b93f6ff4b19d45b62bdb8deec9aac4880b5a618
Lucas Nascimento <lucasvelcks@gmail.com>
2023-08-10 20:57:41 -0300

    Atualização

---
+++ Alura/Array.js
@@ -0,0 +1,26 @@
+const numero1 = 50;
+const numero2 = 43;
+const numero3 = 12;
+
+// utilizando array
+const numeros = [50, 43, 12];
+
+// Alguns tipos
+const arrayDeNumeros = [50, 43, 12];
+const arrayDeStrings = ["banana", "alura", "Juliana"];
+
+// array com 1 elemento
+const numero = [5];
+
+// array com 3 elementos
+const nums = [50, 43, 12];
+
+const num = [10, 12, 20];
+// o número 12 está na posição de número 1 (segunda posição) nessa lista de três números.
+
+// índice          0   1   2   3
+const numIndice = [50, 43, 12, 98];
+
+// índice     0   1   2   3
+const numsLength = [50, 43, 12, 98];
+console.log(numsLength.length) //4
