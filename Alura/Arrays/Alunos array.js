3e273a920bf9a998f879724ee339b5675ad592e1
Lucas Nascimento <lucasvelcks@gmail.com>
2023-08-21 21:54:55 -0300

    Testes array

---
+++ Alura/Arrays/Alunos array.js
@@ -0,0 +1,26 @@
+const Alunos = ['Lucas', 
+'Pedro', 
+'Paulo',
+'josé', 
+'Maria', 
+'Ana', 
+'Thuany', 
+'Nathalia', 
+'Pemela', 
+'Helena', 
+'Julio', 
+'Jean',
+'Carlos',
+'Josefa', 
+'Tyfanny', 
+'Rebeka', 
+'Roberta', 
+'Luana', 
+'Rita', 
+'Marcia'];
+
+const sala1 = Alunos.slice(0, Alunos.slice /2);
+const sala2 = Alunos.slice(Alunos.slice /2);
+console.log(sala1);
+console.log(sala2);
+
--- Alura/Teste Array.js
+++ Alura/Teste Array.js
@@ -1,4 +1,13 @@
+// PUSH
 const notas = [10, 6, 8]
 notas.push(7);
 const Media = (notas[0] + notas[1] + notas[2] + notas[3]) / notas.length ;
 console.log(Media);
+
+// Deletando elementos
+const Notas1 = [10, 6, 8, 5.5, 10]
+Notas1.pop();
+const media1 = (Notas1[0] + Notas1[1] + Notas1[2] + Notas1[3]) / Notas1.length;
+console.log(`A media é ${media1}`);
+
+
